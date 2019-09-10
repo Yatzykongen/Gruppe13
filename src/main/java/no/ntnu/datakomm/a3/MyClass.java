2a983d19461320de4fd61ecbdb54996dccd06219
Sondre Nerhus <sondre.nerhus@hotmail.com>
2019-09-10 12:41:07 +0200

    Endring 2.

--- src/main/java/no/ntnu/datakomm/a3/MyClass.java
+++ src/main/java/no/ntnu/datakomm/a3/MyClass.java
@@ -9,6 +9,7 @@ public class MyClass {
     private static void feature1() {
         // TODO - print "Feature #1"
         // Big boi busta
+        // no blie
     }
 
     private static void feature2() {
